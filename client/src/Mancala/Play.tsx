072cc84f8d44579409d44bb0bec842edfe0f0518
Jorn Bootsma <jbootsma@sogyo.nl>
2021-03-01 09:18:04 +0100

    Game is shown when started and API endpoint created to make a move

--- client/src/Mancala/Play.tsx
+++ client/src/Mancala/Play.tsx
@@ -89,53 +89,5 @@ export function Play({ gameState, setGameState }: PlayProps) {
                 <button disabled={!gameState.players[1].hasTurn}>{gameState.players[1].pits[0].nrOfStones}</button>
             </div>
         </div>
-        // <div className={`grid-container`}>
-        //     {/* <div className="firstLine">{firstLine}</div>
-        //     <div className="onturn">{onturn}</div> */}
-
-        //     <div className="playingbowl0">
-        //         <button onClick={(e) => tryPlayPit(0)} disabled={!gameState.players[0].hasTurn || gameState.gameStatus.endOfGame}>{board[0]}</button>
-        //     </div>
-        //     <div className="playingbowl1">
-        //         <button onClick={(e) => tryPlayPit(1)} disabled={!gameState.players[0].hasTurn || gameState.gameStatus.endOfGame}>{board[1]}</button>
-        //     </div>
-        //     <div className="playingbowl2">
-        //         <button onClick={(e) => tryPlayPit(2)} disabled={!gameState.players[0].hasTurn || gameState.gameStatus.endOfGame}>{board[2]}</button>
-        //     </div>
-        //     <div className="playingbowl3">
-        //         <button onClick={(e) => tryPlayPit(3)} disabled={!gameState.players[0].hasTurn || gameState.gameStatus.endOfGame}>{board[3]}</button>
-        //     </div>
-        //     <div className="playingbowl4">
-        //         <button onClick={(e) => tryPlayPit(4)} disabled={!gameState.players[0].hasTurn || gameState.gameStatus.endOfGame}>{board[4]}</button>
-        //     </div>
-        //     <div className="playingbowl5">
-        //         <button onClick={(e) => tryPlayPit(5)} disabled={!gameState.players[0].hasTurn || gameState.gameStatus.endOfGame}>{board[5]}</button>
-        //     </div>
-        //     <div className="kalaha6">
-        //         <button disabled={!gameState.players[0].hasTurn}>{board[6]}</button>
-        //     </div>
-
-        //     <div className="playingbowl7">
-        //         <button onClick={(e) => tryPlayPit(7)} disabled={!gameState.players[1].hasTurn || gameState.gameStatus.endOfGame}>{board[7]}</button>
-        //     </div>
-        //     <div className="playingbowl8">
-        //         <button onClick={(e) => tryPlayPit(8)} disabled={!gameState.players[1].hasTurn || gameState.gameStatus.endOfGame}>{board[8]}</button>
-        //     </div>
-        //     <div className="playingbowl9">
-        //         <button onClick={(e) => tryPlayPit(9)} disabled={!gameState.players[1].hasTurn || gameState.gameStatus.endOfGame}>{board[9]}</button>
-        //     </div>
-        //     <div className="playingbowl10">
-        //         <button onClick={(e) => tryPlayPit(10)} disabled={!gameState.players[1].hasTurn || gameState.gameStatus.endOfGame}>{board[10]}</button>
-        //     </div>
-        //     <div className="playingbowl11">
-        //         <button onClick={(e) => tryPlayPit(11)} disabled={!gameState.players[1].hasTurn || gameState.gameStatus.endOfGame}>{board[11]}</button>
-        //     </div>
-        //     <div className="playingbowl12">
-        //         <button onClick={(e) => tryPlayPit(12)} disabled={!gameState.players[1].hasTurn || gameState.gameStatus.endOfGame}>{board[12]}</button>
-        //     </div>
-        //     <div className="kalaha13">
-        //         <button disabled={!gameState.players[1].hasTurn}>{board[13]}</button>
-        //     </div>
-        // </div>
     )
 }
